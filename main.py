1690a80d11eb1dcdb5a2a6cfd1be4142b8d797d1
samuel <legosthes@gmail.com>
2025-11-19 23:08:56 +0800

    feat: set up requests to the weather api

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def read_root():
 def get_weather(city: str):
     url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}/next7days?unitGroup=metric&key=HMRFP2UWG2MMCZRAG5LSVKQMH&contentType=json"
     response = requests.get(url)
-    for i in response:
-        print(i)
-    return {"city": city}
+
+    if response.status_code == 200:
+        data = response.json()
+
+        message = f"The weather in {city} is {data['days'][0]['tempmin']}-{data['days'][0]['tempmax']}\u00b0C"
+    else:
+        message = "Data not found."
+    return message
