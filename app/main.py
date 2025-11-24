616020cd56a325dcdf981becee3be0470aef8dfd
samuel <legosthes@gmail.com>
2025-11-24 21:58:12 +0800

    feat: add invalid api parameter error

--- app/main.py
+++ app/main.py
@@ -14,32 +14,38 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/weather/{city}")
-def get_weather(city: str):
+@app.get("/weather/{city}/{start_date}/{end_date}")
+def get_weather(city: str, start_date, end_date):
     BASE_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
-    url = f"{BASE_URL}/{city}/next7days?unitGroup=metric&key={API_KEY}&contentType=json"
+    url = f"{BASE_URL}/{city}/{start_date}/{end_date}?unitGroup=metric&key={API_KEY}&contentType=json&include=days&elements=datetime,tempmax,tempmin,feelslike"
 
     try:
-        cache_key = f"weather:{city}"
+        cache_key = f"weather:{city}, {start_date}"
         cached_data = r.get(cache_key)
         if cached_data:
             return json.loads(cached_data)
 
         response = requests.get(url)
-        data = response.json()
 
         if response.status_code == 200:
+            data = response.json()
+            print(data)
             data_str = json.dumps(data)
             r.set(f"weather:{city}", data_str, ex=CACHE_EXPIRATION)
 
             return data
-        else:
+        elif response.status_code == 400:
             raise HTTPException(
-                status_code=response.status_code,
-                detail=f"Weather API error: {response.status_code}",
+                status_code=404,
+                detail=f"Data is not available. Either {city} is not a valid city.",
+            )
+        elif response.status_code == 429:
+            raise HTTPException(
+                status_code=429,
+                detail="Too many requests",
             )
 
     except requests.exceptions.RequestException as error:
         raise HTTPException(
-            status_code=503, detail=f"Error contacting weather service: {error}"
+            status_code=500, detail=f"Error contacting weather service: {error}"
         )
